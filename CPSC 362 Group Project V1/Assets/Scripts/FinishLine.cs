6c7c1575d5e30ba73940d746def08b50140a49f9
Isaias Soria <89820590+Havocdemon38@users.noreply.github.com>
2024-11-26 19:29:33 -0800

    Update FinishLine.cs

--- CPSC 362 Group Project V1/Assets/Scripts/FinishLine.cs
+++ CPSC 362 Group Project V1/Assets/Scripts/FinishLine.cs
@@ -7,13 +7,8 @@ public class FinishLine : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
-    
+    void Update() 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
