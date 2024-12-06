b0d3a019364e0b4638985ff5b8d348a0b71b7cdd
joebernal <joe.bernal2841@gmail.com>
2024-12-06 00:36:02 -0800

    Add background music and fix Death Script

--- CPSC 362 Group Project V1/Assets/Scripts/DeathScript.cs
+++ CPSC 362 Group Project V1/Assets/Scripts/DeathScript.cs
@@ -6,21 +6,24 @@ public class DeathScript : MonoBehaviour
 {
     public GameObject startPoint;
     public GameObject Player;
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        rb = Player.GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component of the player
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            // Reset player position to the starting point
             Player.transform.position = startPoint.transform.position;
+
+            // Reset player velocity to zero
+            rb.velocity = Vector2.zero;
         }
     }
 }
