b5b9d4df0813b31d750fb00b4f7a735ac71c3724
ChknSndwch <alvarez.erick1597@gmail.com>
2024-12-14 11:43:11 -0800

    Pause Menu
    Added pause to game

---
+++ CPSC 362 Group Project V1/Assets/Scripts/NewPauseMenu.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Only needed if you want to quit or load another scene.
+
+public class NewPauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuPanel;
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuPanel.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
